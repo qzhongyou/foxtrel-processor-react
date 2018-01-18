9c9d24b872b31ffbcde3b6d7b5289af4eb70a594
阙中友 <quezhongyou@zhubajie.com>
2018-01-18 18:15:23 +0800

    配置信息修改

---
+++ README.md
@@ -0,0 +1,6 @@
+## foxtrel-processor-react
+   
+### 文档说明
+
+点击[foxtrel](https://github.com/qzhongyou/foxtrel/blob/master/README.md)到文档说明
+   
---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * @authors       qzhongyou
+ * @date          2017-1-18
+ * @description   create processor
+ */
+
+'use strict';
+
+const webpack = require('webpack');
+
+let config = require('./lib/config');
+
+function processor(options, compilerConfigFun) {
+    //处理器配置信息
+    let webpackConfig = config(options);
+    compilerConfigFun(webpackConfig);
+
+    return webpack(foxtrel.config.get('webpack'));
+}
+
+module.exports = processor;
+
