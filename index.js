39d3c034ec607136dd5c9c7a92a6c25deca089c5
阙中友 <quezhongyou@zhubajie.com>
2018-01-19 18:09:41 +0800

    拆分配置,分为dev和生产环境

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ let config = require('./lib/config');
 function processor(options, compilerConfigFun) {
     //处理器配置信息
     let webpackConfig = config(options);
+
     compilerConfigFun(webpackConfig);
 
+    //返回 compilers
     return webpack(foxtrel.config.get('webpack'));
 }
 
