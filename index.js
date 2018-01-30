ca3566916931f406ad41960c5743ed110241293f
阙中友 <quezhongyou@zhubajie.com>
2018-01-30 15:52:27 +0800

    公共打包方式修改

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const webpack = require('webpack');
 
 let config = require('./lib/config');
 
+const dllConfig = require('./lib/webpack.config.dll');
+
+const async = require('async');
+
 function processor(options, compilerConfigFun) {
+
     //处理器配置信息
     let webpackConfig = config(options);
 
@@ -20,5 +25,24 @@ function processor(options, compilerConfigFun) {
     return webpack(foxtrel.config.get('webpack'));
 }
 
-module.exports = processor;
+
+module.exports = function (callback) {
+    let handler = [];
+    //第三方模块编译
+    if (foxtrel.config.get('resetCommon')) {
+        handler.push(function (cb) {
+            webpack(dllConfig).run(function (error, stats) {
+                if (error || stats.hasErrors()) {
+                    foxtrel.log.error('compilation fails');
+                }
+                cb();
+            })
+        })
+    }
+
+    async.waterfall(handler,function(){
+        callback(processor);
+    })
+}
+
 
