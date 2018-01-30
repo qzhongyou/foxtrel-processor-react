4ee02a14278afc52272702ad45fe14956f4a14be
阙中友 <quezhongyou@zhubajie.com>
2018-01-30 17:43:07 +0800

    版本文件

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ const dllConfig = require('./lib/webpack.config.dll');
 
 const async = require('async');
 
+const ph = require('path');
+
+/**
+ * @description 正常配置编译,返回编译对象
+ * @param options
+ * @param compilerConfigFun
+ */
 function processor(options, compilerConfigFun) {
 
     //处理器配置信息
@@ -26,10 +33,26 @@ function processor(options, compilerConfigFun) {
 }
 
 
+function hasNewstVendor() {
+    let path = dllConfig.output.path,
+        filename = dllConfig.output.filename.replace(/\[name\]/, 'vendor'),
+        filePath = ph.join(path, filename);
+    //是否存在
+    if (foxtrel.util.exists(filePath)) {
+        let pathInfo = foxtrel.util.pathInfo(filePath);
+        let version = pathInfo.filename.match(/@\d+\.?\d+$/)[0];
+        //是否为最新
+        if (version == foxtrel.config.get('vendorVersion')) {
+            return false;
+        }
+    }
+    return true;
+}
+
 module.exports = function (callback) {
     let handler = [];
     //第三方模块编译
-    if (foxtrel.config.get('resetCommon')) {
+    if (hasNewstVendor()) {
         handler.push(function (cb) {
             webpack(dllConfig).run(function (error, stats) {
                 if (error || stats.hasErrors()) {
@@ -40,9 +63,10 @@ module.exports = function (callback) {
         })
     }
 
-    async.waterfall(handler,function(){
+    async.waterfall(handler, function () {
         callback(processor);
     })
 }
+
 
 
