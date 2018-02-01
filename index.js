894039e8d73d278c9bb8789d4723abf128771afe
阙中友 <quezhongyou@zhubajie.com>
2018-02-01 14:20:19 +0800

    输入输出改变 v0.2.2

--- index.js
+++ index.js
@@ -16,45 +16,72 @@ const async = require('async');
 
 const ph = require('path');
 
+const cache = foxtrel.cache;
+
+const flConfig = foxtrel.config;
+
 /**
- * @description 正常配置编译,返回编译对象
- * @param options
- * @param compilerConfigFun
+ * @description 第三方模块是否为最新
+ * @returns {boolean}
  */
-function processor(options, compilerConfigFun) {
-
-    //处理器配置信息
-    let webpackConfig = config(options);
-
-    compilerConfigFun(webpackConfig);
-
-    //返回 compilers
-    return webpack(foxtrel.config.get('webpack'));
-}
-
-
-function hasNewstVendor() {
-    let path = dllConfig.output.path,
-        filename = dllConfig.output.filename.replace(/\[name\]/, 'vendor'),
+function hasNewstVendor(config, options) {
+    let path = config.output.path,
+        filename = config.output.filename.replace(/\[name\]/, 'vendor'),
         filePath = ph.join(path, filename);
     //是否存在
     if (foxtrel.util.exists(filePath)) {
         let pathInfo = foxtrel.util.pathInfo(filePath);
-        let version = pathInfo.filename.match(/@\d+\.?\d+$/)[0];
+        let version = pathInfo.filename.match(/\d+\.?\d+$/)[0];
         //是否为最新
-        if (version == foxtrel.config.get('vendorVersion')) {
+        if (version == options.vendorVersion) {
             return false;
         }
     }
     return true;
 }
 
+/**
+ * @description 编译前,生成配置数据
+ * @param config
+ */
+
+function compilerConfig(config) {
+    foxtrel.util.merge(config, foxtrel.config.get('webpack'));
+    foxtrel.config.set('webpack', config);
+}
+
+
 module.exports = function (callback) {
+
+    const options = {
+        env: flConfig.get('env'),
+        root: foxtrel.project.getProjectRoot(),
+        output: cache.getCachePath(),
+        outputStatic: cache.getCachePath('static'),
+        outputPage: cache.getCachePath('page'),
+        vendorVersion: flConfig.get('vendorVersion') || '1.0',
+        vendor: flConfig.get('vendor') || [],
+        context: foxtrel.project.getProjectRoot('app/views'),
+        publicPath: '/assets/'
+    }
+
+    //第三方配置
+    let outputPath = foxtrel.config.get('webpack.output.path');
+    const _dllConfig = dllConfig(options);
+    if (outputPath) {
+        _dllConfig.output.path = outputPath;
+    }
+
+    //配置信息
+    let webpackConfig = config(options);
+    compilerConfig(webpackConfig);
+
+
     let handler = [];
     //第三方模块编译
-    if (hasNewstVendor()) {
+    if (hasNewstVendor(_dllConfig, options)) {
         handler.push(function (cb) {
-            webpack(dllConfig).run(function (error, stats) {
+            webpack(_dllConfig).run(function (error, stats) {
                 if (error || stats.hasErrors()) {
                     foxtrel.log.error('compilation fails');
                 }
@@ -64,8 +91,9 @@ module.exports = function (callback) {
     }
 
     async.waterfall(handler, function () {
-        callback(processor);
+        callback(webpack(foxtrel.config.get('webpack')));
     })
+
 }
 
 
